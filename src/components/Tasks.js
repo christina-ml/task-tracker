9a7dcdf3722eb8371d64b770f9ae9c90e2c99f97
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-25 12:56:54 -0400

    Added tasks to Task.Js but we want it in our global state instead

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,27 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Oct 25th at 2:20pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at Pursuit',
-        day: 'Oct 27th at 10:00am',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Oct 26th at 1:00pm',
-        reminder: false,
-    }
-]
+import { useState } from 'react'
 
 const Tasks = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'Oct 25th at 2:20pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Meeting at Pursuit',
+            day: 'Oct 27th at 10:00am',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Food Shopping',
+            day: 'Oct 26th at 1:00pm',
+            reminder: false,
+        }
+    ])
+
     return (
         <>
             {tasks.map((task)=> (
