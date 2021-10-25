ad1f4caff46f885378a81d52c3788aadb21d4dee
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-25 11:44:08 -0400

    props.title being passed in from the App.js file

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <div className="container">
-      <Header />
+    <div className='"container"'>
+      <Header title="Hello" />
     </div>
   );
 }
