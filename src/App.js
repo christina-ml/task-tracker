4895471a46cccbf72e78dbe80f18f496b6ae029f
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-25 12:59:08 -0400

    adding tasks into App.js, and then adding the tasks as a prop to Tasks in the return

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useState } from 'react'
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Oct 25th at 2:20pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at Pursuit',
+        day: 'Oct 27th at 10:00am',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Oct 26th at 1:00pm',
+        reminder: false,
+    }
+  ])
+
   return (
     <div className='"container"'>
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,4 @@
-import { useState } from 'react'
-
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Oct 25th at 2:20pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Meeting at Pursuit',
-            day: 'Oct 27th at 10:00am',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Food Shopping',
-            day: 'Oct 26th at 1:00pm',
-            reminder: false,
-        }
-    ])
-
+ const Tasks = () => {
     return (
         <>
             {tasks.map((task)=> (
