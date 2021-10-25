32f08e86efb42e4dec57d2dcc493877c9ae98aec
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-25 12:44:36 -0400

    Creating new Tasks.js component, and adding in 3 tasks as an array of objects

---
+++ src/components/Tasks.js
@@ -0,0 +1,30 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Oct 25th at 2:20pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at Pursuit',
+        day: 'Oct 27th at 10:00am',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Oct 26th at 1:00pm',
+        reminder: false,
+    }
+]
+
+const Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tasks
